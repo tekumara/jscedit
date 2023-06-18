c8411ea20d034769f6fd671d78c16482f047bacb
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-06-18 11:37:53 +1000

    feat: merge interface
    because the old interface required quoting strings, eg:
    
    npx jscedit settings.json foo '"bar"'
    
    which was non-obvious

--- README.md
+++ README.md
@@ -4,18 +4,20 @@ Edit JSONC files eg: VS Code's _settings.json_.
 
 JSONC files are JSON but can contain comments and trailing commas. They are a [VS Code specific file format](https://github.com/microsoft/vscode/blob/beea143b432b485e07f862c55b6c147ae2f5e939/extensions/json-language-features/server/README.md?plain=1#L15).
 
+Uses [microsoft/node-jsonc-parser](https://github.com/microsoft/node-jsonc-parser) which is more or less the [jsonc parser within vscode](https://github.com/microsoft/vscode/blob/43c3107/src/vs/base/common/json.ts).
+
 ## Usage
 
 Add/modify the setting to auto-save files after a delay:
 
 ```sh
-jscedit settings.json files.autoSave afterDelay
+npx jscedit settings.json '{"files.autoSave": "afterDelay"}'
 ```
 
 Read/write from stdin/stdout:
 
 ```sh
-printf '{ # comment\n"foo" : "bar", }' | jscedit - key value
+printf '{ # comment\n"foo" : "bar", }' | npx jscedit - '{ "k1": "v1" }'
 { # comment
 "foo": "bar",
 "key": "value",
--- src/jscedit.ts
+++ src/jscedit.ts
@@ -3,11 +3,7 @@
 import fs from "fs";
 import * as jsonc from "jsonc-parser";
 
-function updateJsoncFile(
-  filePath: string,
-  keyPath: string,
-  value: string
-): void {
+function updateJsoncFile(filePath: string, jsonToMerge: string): void {
   let fileContent: string;
   if (filePath === "-") {
     fileContent = fs.readFileSync("/dev/stdin", "utf8");
@@ -22,15 +18,18 @@ function updateJsoncFile(
     process.exit(42);
   }
 
-  const jsonValue = JSON.parse(value);
+  const jsonValue = JSON.parse(jsonToMerge);
 
-  // Create an edit to update the value
-  const editResult = jsonc.modify(fileContent, [keyPath], jsonValue, {
-    formattingOptions: { tabSize: 2, insertSpaces: true },
-  });
+  // Create edits for every key in jsonValue
+  const edits = Object.keys(jsonValue).flatMap((key) =>
+    // modify is documented here https://github.com/microsoft/node-jsonc-parser/blob/33f744b/src/test/edit.test.ts
+    jsonc.modify(fileContent, [key], jsonValue[key], {
+      formattingOptions: { tabSize: 2, insertSpaces: true },
+    })
+  );
 
   // Apply the edits to the file content
-  const modifiedContent = jsonc.applyEdits(fileContent, editResult);
+  const modifiedContent = jsonc.applyEdits(fileContent, edits);
   if (modifiedContent !== fileContent) {
     if (filePath === "-") {
       process.stdout.write(modifiedContent);
@@ -41,13 +40,10 @@ function updateJsoncFile(
 }
 
 const args = process.argv.slice(2);
-if (args.length !== 3) {
-  console.error(
-    "Usage: jscedit <file.jsonc|- for stdin> <keyPath> <value>"
-  );
+if (args.length !== 2) {
+  console.error("Usage: jscedit <file.jsonc|- for stdin> <json_to_merge>");
 } else {
   const filePath = args[0];
-  const keyPath = args[1];
-  const value = args[2];
-  updateJsoncFile(filePath, keyPath, value);
+  const jsonToMerge = args[1];
+  updateJsoncFile(filePath, jsonToMerge);
 }
